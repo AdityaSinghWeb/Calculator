7623560075e1dad30895c98d5c89249be4bb1edb
AdityaSingh-web18 <adityasingh180710@gmail.com>
2024-07-02 17:01:24 +0530

    1st update

---
+++ calc.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < document.querySelectorAll("#keys>div").length; i++) {
+  document.querySelectorAll("#keys>div")[i].addEventListener("click", cal);
+}
+let storage = "";
+function cal(event) {
+  let keytext = event.target.innerText;
+  if (keytext == "C") {
+    document.querySelector("#display").innerText = "";
+    storage = "";
+  } else if (keytext == "=") {
+    document.querySelector("#display").innerText = eval(storage);
+    if (document.querySelector("#display").innerText == "undefined") {
+      document.querySelector("#display").innerText = "";
+    }
+  } else {
+    storage = storage + keytext;
+    document.querySelector("#display").innerText = storage;
+  }
+}
